98a47a720a90c76f9e4882483fdcef4a7ec8d39f
Jan Werren <jan.werren@outlook.com>
2025-06-13 15:56:15 +0200

    fix: tests

--- src/StreamHub.Web/src/app/features/library/components/library-sidebar/library-sidebar.component.spec.ts
+++ src/StreamHub.Web/src/app/features/library/components/library-sidebar/library-sidebar.component.spec.ts
@@ -2,21 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LibrarySidebarComponent } from './library-sidebar.component';
 import { provideMockStore } from '@ngrx/store/testing';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { ReplaySubject } from 'rxjs';
-import { provideMockActions } from '@ngrx/effects/testing';
 
 describe('LibrarySidebarComponent', () => {
   let component: LibrarySidebarComponent;
   let fixture: ComponentFixture<LibrarySidebarComponent>;
-  let actions$ = new ReplaySubject(1);
 
   TestBed.configureTestingModule({
     imports: [LibrarySidebarComponent],
-    providers: [
-      provideMockStore(),
-      provideHttpClientTesting(),
-      provideMockActions(() => actions$),
-    ],
+    providers: [provideMockStore(), provideHttpClientTesting()],
   });
 
   beforeEach(() => {
