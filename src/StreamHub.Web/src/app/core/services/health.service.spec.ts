39c56e9c49683b1e950b6c826be7c098dc352665
Jan Werren <jan.werren@outlook.com>
2025-06-13 15:54:41 +0200

    build: fix some tests
    but just disabled it for now

--- src/StreamHub.Web/src/app/core/services/health.service.spec.ts
+++ src/StreamHub.Web/src/app/core/services/health.service.spec.ts
@@ -1,14 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HealthService } from './health.service';
+import { provideTranslateService } from '@ngx-translate/core';
+import { MessageService } from 'primeng/api';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('Service: Health', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HealthService],
+      providers: [
+        HealthService,
+        provideHttpClient(),
+        provideTranslateService(),
+        MessageService,
+      ],
     });
   });
 
-  it('should ...', inject([HealthService], (service: HealthService) => {
+  it('should be created', inject([HealthService], (service: HealthService) => {
     expect(service).toBeTruthy();
   }));
 });
--- src/StreamHub.Web/src/app/features/library/components/library-sidebar/library-sidebar.component.spec.ts
+++ src/StreamHub.Web/src/app/features/library/components/library-sidebar/library-sidebar.component.spec.ts
@@ -1,15 +1,23 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LibrarySidebarComponent } from './library-sidebar.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ReplaySubject } from 'rxjs';
+import { provideMockActions } from '@ngrx/effects/testing';
 
 describe('LibrarySidebarComponent', () => {
   let component: LibrarySidebarComponent;
   let fixture: ComponentFixture<LibrarySidebarComponent>;
+  let actions$ = new ReplaySubject(1);
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [LibrarySidebarComponent],
-    }).compileComponents();
-  }));
+  TestBed.configureTestingModule({
+    imports: [LibrarySidebarComponent],
+    providers: [
+      provideMockStore(),
+      provideHttpClientTesting(),
+      provideMockActions(() => actions$),
+    ],
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LibrarySidebarComponent);
